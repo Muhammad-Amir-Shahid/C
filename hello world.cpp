b33cc3be5f04a928ba1643813870cc9252483dd4
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-26 22:23:31 +0500

    arrays

---
+++ hello world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+    int array[5];
+    for(int i=0;i<3;i++)
+    {
+        cin>>array[i];
+    }
+ for(int i=0;i<3;i++)
+    {
+        cout<<array[i];
+    }
+}
