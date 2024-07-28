13d79b39469c64b36647c43bc8d3082d46ba9a17
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-07-28 23:12:13 +0500

    inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class A{
+    public:
+    void func()
+    {
+        cout<<"A";
+    }
+};
+
+class B:public A
+{
+public:
+void func ()
+{
+    cout<<"B";
+}
+
+};
+
+void main()
+{
+B b;
+b.func();
+
+}
