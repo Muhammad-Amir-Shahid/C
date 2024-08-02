f304d4b81aefe49e5ce8d3e92591e6d35ea3cfc0
Muhammad Amir Shahid <bulcpers0n0l@gmail.com>
2024-08-02 21:48:38 +0500

    classes

---
+++ classes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class student{
+char name[20];
+int roll_no;
+char section[10];
+
+public:
+void getName();
+void setName();
+void getRoll();
+void setRoll();
+void getSection();
+void setSection();
+
+};
+
+int main()
+{return 0;}
